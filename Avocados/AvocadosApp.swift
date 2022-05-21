431151ab3d2289ccb4a56aef353f8828dd1c1848
newlandslucas <lucasnewlands.m@gmail.com>
2022-05-21 14:30:59 -0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Lucas Newlands on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
