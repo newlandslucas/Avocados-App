6fc72705622766c3cf40b17f4edfb44ff402f182
newlandslucas <lucasnewlands.m@gmail.com>
2022-05-22 17:19:56 -0300

    feat: HeaderModel | Header Data

---
+++ Avocados/Data/HeaderData.swift
@@ -0,0 +1,43 @@
+//
+//  HeaderData.swift
+//  Avocados
+//
+//  Created by Lucas Newlands on 22/05/22.
+//
+
+import SwiftUI
+
+// MARK: - HEADER DATA
+
+let headersData: [Header] = [
+    Header(
+      image: "avocado-slice-1",
+      headline: "Abacates",
+      subheadline: "Os abacates são um ingrediente poderoso em qualquer refeição para qualquer pessoa."
+    ),
+    Header(
+      image: "avocado-slice-2",
+      headline: "Saudável",
+      subheadline: "Os abacates são bons para nós e podem fazer parte da nossa dieta saudável."
+    ),
+    Header(
+      image: "avocado-slice-3",
+      headline: "Nutrientes",
+      subheadline: "Os abacates têm uma quantidade e diversidade surpreendentes de vitaminas e minerais."
+    ),
+    Header(
+      image: "avocado-slice-4",
+      headline: "Deliciosos",
+      subheadline: "Quer mais guacamole? Encontre as melhores receitas de guacamole no app."
+    ),
+    Header(
+      image: "avocado-slice-5",
+      headline: "Gostosos",
+      subheadline: "Abacates são uma boa fonte de fibra natural, que faz você se sentir satisfeito."
+    ),
+    Header(
+      image: "avocado-slice-6",
+      headline: "Receitas",
+      subheadline: "Aproveite estas receitas de abacate cuidadosamente escolhidas a dedo para todas as ocasiões!"
+    )
+]
---
+++ Avocados/Model/HeaderModel.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderModel.swift
+//  Avocados
+//
+//  Created by Lucas Newlands on 22/05/22.
+//
+
+import SwiftUI
+
+//MARK: - HEADER MODEL
+
+struct Header: Identifiable {
+    var id = UUID()
+    var image: String
+    var headline: String
+    var subheadline: String
+    
+}
